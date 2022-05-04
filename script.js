592b93c5e9c1fbc22b22842271f5621edfd090cc
Adam Lindley <adamlindley98@gmail.com>
2022-05-04 17:13:05 +0100

    Prompt for grid size -keeps perfect ratio -Only allows user input from 1 to 100

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 const container = document.querySelector("#container");
+let gridSize = 256;
+let squares = 16;
 
-for(i=1;i<=256;i++){
+
+for(i=1;i<=gridSize;i++){
     const div = document.createElement("div");
     container.appendChild(div);
-    //divs.textContent = ("hello");
-    //div.style.border = "solid black";
-    div.style.height = "50px";
-    div.style.width = "50px";
-    div.addEventListener("mouseover",() =>{
+    let height = 800/squares;
+    let width = 800/squares;
+    div.style.height = height + "px";
+    div.style.width = width + "px";
+    div.addEventListener("mouseover", () =>{
         div.style.backgroundColor = "grey";
     });
     div.addEventListener("mouseout", () => {
         div.style.backgroundColor = "black";
     });
 };
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", numberSquares);
+
+function numberSquares(){
+    let squares = prompt("Number of squares per side");
+    gridSize = squares * squares;
+    console.log(gridSize);
+    if(squares<1 || squares>100){
+        alert("Value must be bigger than 0 and smaller than 100")
+    }else{
+        while(container.hasChildNodes()){
+            container.removeChild(container.firstChild);
+        }
+        for (i = 1; i <= gridSize; i++) {
+            const div = document.createElement("div");
+            container.appendChild(div);
+            let height = 800 / squares;
+            let width = 800 / squares;
+            div.style.height = height + "px";
+            div.style.width = width + "px";
+            //div.style.height = "50px";
+            //div.style.width = "50px";
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = "grey";
+            });
+            div.addEventListener("mouseout", () => {
+                div.style.backgroundColor = "black";
+            });
+        };
+    }
+}
