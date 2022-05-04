2ce60526c37d1e3b02caa9bff488eda6479e1505
Adam Lindley <adamlindley98@gmail.com>
2022-05-04 13:42:03 +0100

    Hover effect and page css

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const container = document.querySelector("#container");
 
 for(i=1;i<=256;i++){
-    const divs = document.createElement("div");
-    container.appendChild(divs);
+    const div = document.createElement("div");
+    container.appendChild(div);
     //divs.textContent = ("hello");
-    divs.style.border = "solid black";
-    divs.style.height = "3rem";
-    divs.style.width = "3rem";
-}
-
+    //div.style.border = "solid black";
+    div.style.height = "50px";
+    div.style.width = "50px";
+    div.addEventListener("mouseover",() =>{
+        div.style.backgroundColor = "grey";
+    });
+    div.addEventListener("mouseout", () => {
+        div.style.backgroundColor = "black";
+    });
+};
