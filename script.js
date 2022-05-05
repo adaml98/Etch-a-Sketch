e5b10cc1e2a36a4c303d80bad5811013c65ffbdd
Adam Lindley <adamlindley98@gmail.com>
2022-05-05 21:00:53 +0100

    Cleaned up code.

--- script.js
+++ script.js
@@ -1,51 +1,40 @@
-const container = document.querySelector("#container");
-let gridSize = 256;
-let squares = 16;
 
-
-for(i=1;i<=gridSize;i++){
-    const div = document.createElement("div");
-    container.appendChild(div);
-    let height = 800/squares;
-    let width = 800/squares;
-    div.style.height = height + "px";
-    div.style.width = width + "px";
-    div.addEventListener("mouseover", () =>{
-        div.style.backgroundColor = "grey";
-    });
-    div.addEventListener("mouseout", () => {
-        div.style.backgroundColor = "black";
-    });
+function createGrid(){
+    for (i = 1; i <= gridSize; i++) {
+        const div = document.createElement("div");
+        container.appendChild(div);
+        let side = 800 / squares;
+        div.style.height = side + "px";
+        div.style.width = side + "px";
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "grey";
+        });
+        div.addEventListener("mouseout", () => {
+            div.style.backgroundColor = "black";
+        });
+    };
 };
 
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", numberSquares);
 
 function numberSquares(){
-    let squares = prompt("Number of squares per side");
+    squares = prompt("Number of squares per side");
     gridSize = squares * squares;
-    console.log(gridSize);
     if(squares<1 || squares>100){
-        alert("Value must be bigger than 0 and smaller than 100")
+        alert("Must be between 1 and 100")
     }else{
         while(container.hasChildNodes()){
             container.removeChild(container.firstChild);
         }
-        for (i = 1; i <= gridSize; i++) {
-            const div = document.createElement("div");
-            container.appendChild(div);
-            let height = 800 / squares;
-            let width = 800 / squares;
-            div.style.height = height + "px";
-            div.style.width = width + "px";
-            //div.style.height = "50px";
-            //div.style.width = "50px";
-            div.addEventListener("mouseover", () => {
-                div.style.backgroundColor = "grey";
-            });
-            div.addEventListener("mouseout", () => {
-                div.style.backgroundColor = "black";
-            });
-        };
+        createGrid();
     }
 }
+
+let gridSize = 256;
+let squares = 16;
+
+const container = document.querySelector("#container");
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", numberSquares);
+
+createGrid();
+
